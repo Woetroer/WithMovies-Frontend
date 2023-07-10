c0a95168e83bae5afdc946bc6e88219f39c59c62
hugo4it <hugo.vandekuilen@itvitaelearning.nl>
2023-07-10 14:00:35 +0200

    Removed ITitle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ITitle } from '../interfaces/ITitle';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements ITitle {
-    pageTitle = "Home";
+export class HomeComponent {
 }
