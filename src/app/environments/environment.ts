b381366efc913148f8ccddbbdfdcd28800051dde
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-07-25 10:39:43 +0200

    Adult changed into a genre for ease of use

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     tmdbApiKey: "5e022855870ce89d55b1c13e2e028395",
     tmdbApiAccessToken:
         "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTAyMjg1NTg3MGNlODlkNTViMWMxM2UyZTAyODM5NSIsInN1YiI6IjY0YjUzYzdiNzg1NzBlMDEzYTkyOGNmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DK7lEZAC3KOBljH88H_yXMq0ko8ucwGQoxQiXYvYni4",
-    genres: ["Crime", "Adventure", "Documentary", "Fantasy", "ScienceFiction", "Mystery", "Romance", "Western", "Horror", "Drama", "Comedy", "Action", "War", "Music", "TvMovie", "History", "Thriller", "Animation", "Family", "Foreign"],
+    genres: ["Adult", "Crime", "Adventure", "Documentary", "Fantasy", "ScienceFiction", "Mystery", "Romance", "Western", "Horror", "Drama", "Comedy", "Action", "War", "Music", "TvMovie", "History", "Thriller", "Animation", "Family", "Foreign"],
 };
