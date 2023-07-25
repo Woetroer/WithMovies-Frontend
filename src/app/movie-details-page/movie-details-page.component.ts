9997ae4c395cafa5ecbb283127f93530db930622
hugo4it <hugo.vandekuilen@itvitaelearning.nl>
2023-07-25 09:15:25 +0200

    moving to my pc

--- src/app/movie-details-page/movie-details-page.component.ts
+++ src/app/movie-details-page/movie-details-page.component.ts
@@ -1,74 +1,116 @@
-import { Component, Input } from '@angular/core';
-import { IMovieDto } from 'src/interfaces/IMovieDto';
-import { MovieService } from '../services/movie.service';
-import { faStar, faUsersLine } from '@fortawesome/free-solid-svg-icons';
-import { ImageQuality, MovieImageType, TmdbService } from '../services/tmdb.service';
+import { Component, Input } from "@angular/core";
+import { IMovieDto } from "src/interfaces/IMovieDto";
+import { MovieService } from "../services/movie.service";
+import { faStar, faUsersLine } from "@fortawesome/free-solid-svg-icons";
+import {
+    ImageQuality,
+    MovieImageType,
+    TmdbService,
+} from "../services/tmdb.service";
 
 @Component({
-  selector: 'app-movie-details-page',
-  templateUrl: './movie-details-page.component.html',
-  styleUrls: ['./movie-details-page.component.scss']
+    selector: "app-movie-details-page",
+    templateUrl: "./movie-details-page.component.html",
+    styleUrls: ["./movie-details-page.component.scss"],
 })
 export class MovieDetailsPageComponent {
-  faStar = faStar;
-  faUsersLine = faUsersLine;
+    faStar = faStar;
+    faUsersLine = faUsersLine;
 
-  public id!: number;
-  public movie?: IMovieDto;
+    public id!: number;
+    public movie?: IMovieDto;
 
-  public posterPath: string = "";
-  public backdropPath: string = "";
+    public posterPath: string = "";
+    public backdropPath: string = "";
 
-  public lastHovered: number = 0;
+    public lastHovered: number = 0;
 
-  public companyNames = "";
+    public companyNames = "";
 
-  public dateOfRelease = "";
-  public languageNames?: string;
-  public genreNames!: string;
-  public budgetDisplay: string = "";
-  public runtimeDisplay: string = "";
+    public dateOfRelease = "";
+    public languageNames?: string;
+    public genreNames!: string;
+    public budgetDisplay: string = "";
+    public runtimeDisplay: string = "";
 
-  private languageNameProvider =  new Intl.DisplayNames(['en'], { type: 'language' });
-
-  @Input("id") set setId(newId: number) {
-    this.id = newId;
-    this.fetchMovie();
-  }
-
-  constructor(private movieService: MovieService, private tmdbService: TmdbService){}
-
-  fetchMovie() {
-    this.movieService.getMovieDetails(this.id).subscribe((movie: IMovieDto) => {
-      
-      this.movie = movie;
-      this.companyNames = [...this.movie.productionCompanies ?? []].map((c) => c.name).join(", ");
-      this.dateOfRelease = new Date(movie.releaseDate as any).toLocaleDateString();
-      this.languageNames = [...movie.spokenLanguages ?? []].map(l => this.languageNameProvider.of(l)).join(', ');
-      this.genreNames  = this.movieService.getGenreNames(movie.genres as any);
-      this.movieService.convertMStoHM(movie.runtime as any);
-      this.budgetDisplay = new Intl.NumberFormat(["en-US"], { style: "currency", currency: "USD" }).format(this.movie.budget);
-      this.runtimeDisplay = this.movieService.convertMStoHM(this.movie.runtime);
+    private languageNameProvider = new Intl.DisplayNames(["en"], {
+        type: "language",
     });
 
-    this.tmdbService.getMovieImage(this.id, MovieImageType.Poster, ImageQuality.Original).subscribe((poster) => this.posterPath = poster);
-    this.tmdbService.getMovieImage(this.id, MovieImageType.Poster, ImageQuality.Original).subscribe((backdrop) => this.backdropPath = backdrop);
-  }
+    public popupOpen = false;
+    public ratingScore = 0;
+    public ratingMessage = "";
 
-  onHoverStar(index: number) {
-    this.lastHovered = index;
-  }
+    @Input("id") set setId(newId: number) {
+        this.id = newId;
+        this.fetchMovie();
+    }
 
-  giveRating() {
-    
-    let rating = this.lastHovered;
+    constructor(
+        private movieService: MovieService,
+        private tmdbService: TmdbService
+    ) {}
 
-  }
+    fetchMovie() {
+        this.movieService
+            .getMovieDetails(this.id)
+            .subscribe((movie: IMovieDto) => {
+                this.movie = movie;
+                this.companyNames = [...(this.movie.productionCompanies ?? [])]
+                    .map((c) => c.name)
+                    .join(", ");
+                this.dateOfRelease = new Date(
+                    movie.releaseDate as any
+                ).toLocaleDateString();
+                this.languageNames = [...(movie.spokenLanguages ?? [])]
+                    .map((l) => this.languageNameProvider.of(l))
+                    .join(", ");
+                this.genreNames = this.movieService.getGenreNames(
+                    movie.genres as any
+                );
+                this.movieService.convertMStoHM(movie.runtime as any);
+                this.budgetDisplay = new Intl.NumberFormat(["en-US"], {
+                    style: "currency",
+                    currency: "USD",
+                }).format(this.movie.budget);
+                this.runtimeDisplay = this.movieService.convertMStoHM(
+                    this.movie.runtime
+                );
+            });
 
-  getCollectionName(original: string) {
-    let words = original.split(' ');
-    if (words.at(0)?.toLocaleLowerCase() != "the" && words.at(-1)?.toLocaleLowerCase() == "collection")
-      return "the " + original;
-    return original;
-  }
+        this.tmdbService
+            .getMovieImage(
+                this.id,
+                MovieImageType.Poster,
+                ImageQuality.Original
+            )
+            .subscribe((poster) => (this.posterPath = poster));
+        this.tmdbService
+            .getMovieImage(
+                this.id,
+                MovieImageType.Backdrop,
+                ImageQuality.Original
+            )
+            .subscribe((backdrop) => (this.backdropPath = backdrop));
+    }
+
+    onHoverStar(index: number) {
+        this.lastHovered = index;
+    }
+
+    giveRating() {
+        this.ratingScore = this.lastHovered;
+
+        this.popupOpen = true;
+    }
+
+    getCollectionName(original: string) {
+        let words = original.split(" ");
+        if (
+            words.at(0)?.toLocaleLowerCase() != "the" &&
+            words.at(-1)?.toLocaleLowerCase() == "collection"
+        )
+            return "the " + original;
+        return original;
+    }
 }
