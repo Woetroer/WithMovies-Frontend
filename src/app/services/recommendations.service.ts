5ab3460f1208bf2d3edee4f60760f04324e565e6
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-07-27 14:01:34 +0200

    made the api-url the same as backend

--- src/app/services/recommendations.service.ts
+++ src/app/services/recommendations.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Input } from '@angular/core';
-import { PreferenceComponent } from '../preference/preference.component';
 import { environment } from '../environments/environment';
-import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -12,6 +10,6 @@ export class RecommendationsService {
     constructor(private httpClient: HttpClient) { }
 
     sendPreferences(preferences: Record<string, boolean>) {
-        return this.httpClient.post<any>(environment.apiUrl + "recommendation/sendPreferences", { preferences })
+        return this.httpClient.post<any>(environment.apiUrl + "recommendation/preferences", { preferences })
     }
 }
