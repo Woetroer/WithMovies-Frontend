84198d65a0733bf5078183051844c615ba82d9fc
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-07-31 15:30:19 +0200

    made analytics service and request

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,8 +1,36 @@
 import { Component } from "@angular/core";
+import { AnalyticsService } from "../services/analytics.service";
+import { MoviePreview } from "src/interfaces/MoviePreview";
+import { environment } from "../environments/environment";
+import { Genre } from "src/interfaces/Genre";
+
+type MovieKind = "trending" | "trending-recommended" | "friends" | "watchlist";
+
 
 @Component({
     selector: "app-analytics",
     templateUrl: "./analytics.component.html",
     styleUrls: ["./analytics.component.scss"],
 })
-export class AnalyticsComponent {}
+export class AnalyticsComponent {
+
+movies = new Map<MovieKind, MoviePreview[]>();
+genres = environment.genres;
+
+trendingGenresInInt: number[] = [];
+
+trendingGenre: Genre[] = ;
+
+//genreOrder = for each number in trendingGenres: genres[number] add to trending  
+
+constructor(private analyticsService: AnalyticsService){}
+
+getTendingMovies(){
+    this.analyticsService.getTrending(0, 10).subscribe();
+}
+
+getTrendingGenres(){
+    this.analyticsService.getTrendingGenres(0, 10).subscribe();
+}
+
+}
---
+++ src/app/services/analytics.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IndexRange, LazyLoadedArray } from 'src/LazyLoadedArray';
+import { toObservable } from 'src/ToObservable';
+import { environment } from '../environments/environment';
+import { MoviePreview } from 'src/interfaces/MoviePreview';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+  static client: HttpClient;
+
+  trendingMovies: LazyLoadedArray<MoviePreview>;
+  trendingGenres: number[] = [];
+  
+  constructor(private httpClient: HttpClient) {
+  AnalyticsService.client = httpClient;
+  
+  this.trendingMovies = new LazyLoadedArray(AnalyticsService._getTrending);
+  }
+
+public getTrending(start: number, limit: number) {
+return toObservable(
+    this.trendingMovies.getRange(new IndexRange(start, start + limit))
+);
+}
+
+private static _getTrending(range: IndexRange) {
+return new Promise<MoviePreview[]>((res) =>
+    AnalyticsService.client
+        .get<MoviePreview[]>(
+            environment.apiUrl +
+                `movie/trending/${range.start}/${range.count()}`
+        )
+        .subscribe(res)
+);
+}
+
+public getTrendingGenres(start: number, limit: number) {
+  return AnalyticsService.client.get<Number[]>( 
+  environment.apiUrl + `movie/trending/genres/${start}/${limit}` );
+  }
+}
