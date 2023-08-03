99b3be124b556a2ae6336fb33b4045aa5078eeab
Ivo Dijkman <ivo.dijkman@itvitaelearning.nl>
2023-08-03 12:26:16 +0200

    methods for user info analytics

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { AnalyticsService } from "../services/analytics.service";
 import { MoviePreview } from "src/interfaces/MoviePreview";
 import { environment } from "../environments/environment";
+import { User } from "src/interfaces/User";
 
 type MovieKind = "trending";
 
@@ -14,9 +15,13 @@ type MovieKind = "trending";
 export class AnalyticsComponent {
 
     movies = new Map<MovieKind, MoviePreview[]>();
-    genres = environment.genres;
 
+    genres = environment.genres;
     trendingGenres: string[] = [];
+
+    allUsers: number = 0;
+    averageReviewsPerUser: number = 0;
+    mostActiveUsers: User[] = [];
 
     //genreOrder = for each number in trendingGenres: genres[number] add to trending  
 
@@ -31,5 +36,16 @@ export class AnalyticsComponent {
             this.trendingGenres = res.map(i => this.genres[i]);
         });    
     };
+
+    public getAllUsers() {
+        this.analyticsService.getAllUsers().subscribe(res => this.allUsers)
+    };
+    public getAverageReviewsPerUser() {
+        this.analyticsService.getAverageReviewsPerUser().subscribe(res => this.averageReviewsPerUser)
+    };
+    public getUsersWithMostReviews(amount: number) {
+        this.analyticsService.getUsersWithMostReviews(10).subscribe(res => this.mostActiveUsers)
+    };
+    
 }
                 
